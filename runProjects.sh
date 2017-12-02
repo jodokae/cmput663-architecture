b0cf22c4684a9d1a7ffa29693649a5191ccc8356
Johannes <sir_stormrider@web.de>
2017-12-02 13:48:38 -0700

    run all projects with one script

---
+++ runProjects.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+java -jar architecture-0.0.1-SNAPSHOT-shaded.jar SonarSource/sonarqube  0
+java -jar architecture-0.0.1-SNAPSHOT-shaded.jar Graylog2/graylog2-server 0
+java -jar architecture-0.0.1-SNAPSHOT-shaded.jar square/okhttp 0
+java -jar architecture-0.0.1-SNAPSHOT-shaded.jar CloudifySource/cloudify 0
+java -jar architecture-0.0.1-SNAPSHOT-shaded.jar structr/structr  0
+java -jar architecture-0.0.1-SNAPSHOT-shaded.jar owlcs/owlapi  0
+java -jar architecture-0.0.1-SNAPSHOT-shaded.jar jOOQ/jOOQ 0
+java -jar architecture-0.0.1-SNAPSHOT-shaded.jar checkstyle/checkstyle 0
+java -jar architecture-0.0.1-SNAPSHOT-shaded.jar mikera/vectorz 0
+java -jar architecture-0.0.1-SNAPSHOT-shaded.jar datastax/java-driver 0
